8ccda8f562df3ef55823ad9dccef4bc2548165b1
Rodolfo Gutierrez <rag.ortega@gmail.com>
2020-12-23 13:49:05 -0600

    Termino unidad 03

---
+++ src/CounterApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+
+    const [counter, setCounter] = useState(value);
+
+    // handleAdd
+    const handleAdd = () => {
+        setCounter(counter + 1);
+    };
+
+    const handleSubtract = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
+
+    return (<>
+        <h1>CounterApp</h1>
+        <h2>{counter}</h2>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubtract}>-1</button>
+    </>);
+};
+
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+CounterApp.defaultProps = {
+    value: 0,
+}
+
+
+export default CounterApp;
